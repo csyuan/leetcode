480a242e714c44b4faad344afc2444fb2ade60f9
Sl.Yuan <cs.yuan777@gmail.com>
2017-03-14 22:05:48 +0800

    add scala

--- Algorithms/src/main/scala/WeekOne.scala
+++ Algorithms/src/main/scala/WeekOne.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 /**
   * Created by Sl.Yuan on 2017/3/14.
   */
@@ -15,7 +17,38 @@ object WeekOne {
     for(i <- Range(10,-1,-1)) println(i)
   }
 
+  def countDown(n:Int) : Unit = {
+    for(i <- Range(n-1,-1,-1)) println(i)
+
+  }
+
+  def exchangeElem(array:Array[Int]) = {
+    for(idx <- 0 until (if(array.length % 2 == 0) array.length else array.length -1 ,2) ){
+      val tmp = array(idx)
+      array(idx) = array(idx + 1)
+      array(idx + 1) = tmp
+    }
+    array
+  }
+
+
+  def wordCount(content : String) : Map[String, Int] = {
+    val countMap = scala.collection.mutable.Map[String,Int]()
+
+    for(word <- content.split(" ")) {
+      if (countMap.contains(word))  {
+        countMap(word) = countMap(word) + 1
+      } else {
+        countMap += (word ->1)
+      }
+    }
+
+    countMap.toMap
+  }
+
   def main(args: Array[String]): Unit = {
-    printNum()
+//    countDown(10)
+    for(x <- exchangeElem(Array(2,3,4,5,6,7,8,9))) print(x + " ")
+
   }
 }
