d0846a8cdd9525780dd0df250dd3269cc40a2d5c
slyuan <cs.yuan777@hotmail.com>
2017-03-16 22:48:46 +0800

    basic

--- Algorithms/src/main/scala/WeekTwo.scala
+++ Algorithms/src/main/scala/WeekTwo.scala
@@ -5,13 +5,9 @@ class WeekTwo {
 
 }
 
-object WeekTwo extends App {
-  val p = Point(3,4)
-  println(p.toString)
-  println(p.x + p.y)
-}
 
 
+//one
 class BankAccount {
 
 
@@ -31,6 +27,7 @@ class BankAccount {
   def balance = account
 }
 
+//two
 class Person4(info:String) {
   val _name = info.split("\\s+")
   val firstName = _name(0)
@@ -41,12 +38,43 @@ class Person4(info:String) {
 }
 
 
+////three
+//class Point(val x:Int, val y:Int) {
+//  override def toString = "Point x : " + x + ", y : " + y
+//}
+//
+//object Point{
+//  def apply(x: Int, y: Int): Point = new Point(x, y)
+//}
 
-class Point(val x:Int, val y:Int) {
-  override def toString = "Point x : " + x + ", y : " + y
+
+
+final class point(val x: Int, val y: Int) {
+  override def toString: String = s"(x,y) :($x,$y)"
 }
 
-object Point{
-  def apply(x: Int, y: Int): Point = new Point(x, y)
+
+//four
+abstract class Shape {
+  def centerPoint :point
+  def className = this.getClass.getSimpleName
+
+  override def toString: String = s"Shape=>$className, Center=>$centerPoint"
+
 }
+
+class Rectangle(val topLeft:point,val bottomRight:point) extends Shape {
+
+  override def centerPoint = new point((bottomRight.x + topLeft.x) / 2, (bottomRight.y + topLeft.y) / 2)
+
+}
+
+class Circle(val centerPoint: point, val radius: Int) extends Shape
+
+object WeekTwo extends App {
+  val rect = new Rectangle(new point(0,0), new point(10, 10))
+  println(rect)
+}
+
+
 
